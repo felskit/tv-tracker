b942b08460b7ee582c1c3a0e68345f9d59b59930
andrzejbg <jacekdziwulski@gmail.com>
2017-04-17 21:50:07 +0200

    Fixed screen rotation, added snackbar shown after pressing fab.

--- TVTracker/app/src/main/java/android/tvtracker/SeriesFragment.java
+++ TVTracker/app/src/main/java/android/tvtracker/SeriesFragment.java
@@ -2,6 +2,8 @@ package android.tvtracker;
 
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -17,8 +19,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class SeriesFragment extends Fragment {
-    private TextView mSeriesTitle;
-    private TextView mSeriesDescription;
     private ImageView mSeriesImage;
     private CollapsingToolbarLayout mToolbarLayout;
 
@@ -36,37 +36,10 @@ public class SeriesFragment extends Fragment {
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
-        //mSeriesTitle = (TextView) view.findViewById(R.id.series_title);
-        //mSeriesDescription = (TextView) view.findViewById(R.id.series_details);
         mSeriesImage = (ImageView) view.findViewById(R.id.series_image);
         mToolbarLayout = (CollapsingToolbarLayout) view.findViewById(R.id.collapsing);
 
-        //mSeriesTitle.setText("The Office");
         mToolbarLayout.setTitle("The Office");
-
-//        mSeriesDescription.setText("The Office is an American television comedy series that aired on" +
-//                " NBC from March 24, 2005 to May 16, 2013.[1] It is an adaptation of the BBC series " +
-//                "of the same name. The Office was adapted for American audiences by Greg Daniels, a" +
-//                " veteran writer for Saturday Night Live, King of the Hill, and The Simpsons. It is" +
-//                " co-produced by Daniels' Deedle-Dee Productions, and Reveille Productions (later Shine America), " +
-//                "in association with Universal Television. The original executive producers were Greg Daniels," +
-//                " Howard Klein, Ben Silverman, Ricky Gervais, and Stephen Merchant, with numerous others" +
-//                " being promoted in later seasons.\n" +
-//                "\n" +
-//                "The series depicts the everyday lives of office employees in the Scranton, Pennsylvania," +
-//                " branch of the fictional Dunder Mifflin Paper Company. To simulate the look of an actual documentary," +
-//                " it is filmed in a single-camera setup, without a studio audience or a laugh track. " +
-//                "The show debuted on NBC as a mid-season replacement and ran for nine seasons and 201 episodes." +
-//                " The Office initially featured Steve Carell, Rainn Wilson, John Krasinski, Jenna Fischer " +
-//                "and B. J. Novak as the main cast; the show experienced numerous changes to its ensemble " +
-//                "cast during its run.\n" +
-//                "\n" +
-//                "The first season of The Office was met with mixed reviews, but the following four seasons " +
-//                "received widespread acclaim from television critics. These seasons were included on several" +
-//                " critics' year-end top TV series lists, winning several awards including four Primetime " +
-//                "Emmy Awards, including Outstanding Comedy Series in 2006. While later seasons were criticized" +
-//                " for a decline in quality, earlier writers oversaw the final season and ended the " +
-//                "show's run with a positive reception.");
 
         mSeriesImage.setScaleType(ImageView.ScaleType.CENTER);
         new ImageDownloader(mSeriesImage).execute("https://lh4.googleusercontent.com/-g4OI1bwTNIA/AAAAAAAAAAI/AAAAAAAABt8/aSmWUXBm2bk/s0-c-k-no-ns/photo.jpg");
@@ -75,6 +48,14 @@ public class SeriesFragment extends Fragment {
         ViewPager viewPager = (ViewPager) view.findViewById(R.id.materialup_viewpager);
         viewPager.setAdapter(new TabsAdapter(getChildFragmentManager()));
         tabLayout.setupWithViewPager(viewPager);
+
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.series_subscribe);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "Series added to favourites", Snackbar.LENGTH_LONG).show();
+            }
+        });
     }
 
     private static class TabsAdapter extends FragmentPagerAdapter {
@@ -91,7 +72,6 @@ public class SeriesFragment extends Fragment {
 
         @Override
         public Fragment getItem(int i) {
-            //return CalendarFragment.newInstance("a", "a");
             switch (i) {
                 case 0:
                     return new SeriesDetailsFragment();
